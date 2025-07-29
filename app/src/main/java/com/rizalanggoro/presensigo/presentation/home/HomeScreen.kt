881372574f4a98266ee5e19e02949c110e7d624d
Rizal Anggoro <gnoogler4@gmail.com>
2025-07-29 08:50:20 +0700

    chore: redesign setting screen

--- app/src/main/java/com/rizalanggoro/presensigo/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/rizalanggoro/presensigo/presentation/home/HomeScreen.kt
@@ -2,6 +2,13 @@ package com.rizalanggoro.presensigo.presentation.home
 
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Checklist
+import androidx.compose.material.icons.filled.MoreTime
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Checklist
+import androidx.compose.material.icons.outlined.MoreTime
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -20,10 +27,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.composables.icons.lucide.ListChecks
-import com.composables.icons.lucide.ListX
-import com.composables.icons.lucide.Lucide
-import com.composables.icons.lucide.Settings
 import com.rizalanggoro.presensigo.core.Routes
 import com.rizalanggoro.presensigo.presentation.home.attendance.HomeAttendanceScreen
 import com.rizalanggoro.presensigo.presentation.home.lateness.HomeLatenessScreen
@@ -32,23 +35,27 @@ import com.rizalanggoro.presensigo.presentation.home.setting.HomeSettingScreen
 private data class NavigationItem<T : Any>(
     val title: String,
     val icon: ImageVector,
+    val iconSelected: ImageVector,
     val route: T
 )
 
 private val navigationItems = listOf(
     NavigationItem(
         title = "Presensi",
-        icon = Lucide.ListChecks,
+        icon = Icons.Outlined.Checklist,
+        iconSelected = Icons.Filled.Checklist,
         route = Routes.Home.Attendance
     ),
     NavigationItem(
         title = "Keterlambatan",
-        icon = Lucide.ListX,
+        icon = Icons.Outlined.MoreTime,
+        iconSelected = Icons.Filled.MoreTime,
         route = Routes.Home.Lateness
     ),
     NavigationItem(
         title = "Pengaturan",
-        icon = Lucide.Settings,
+        icon = Icons.Outlined.Settings,
+        iconSelected = Icons.Filled.Settings,
         route = Routes.Home.Setting
     )
 )
@@ -82,7 +89,14 @@ fun HomeScreen() {
                                 restoreState = true
                             }
                         },
-                        icon = { Icon(item.icon, contentDescription = null) },
+                        icon = {
+                            Icon(
+                                when (isSelected) {
+                                    true -> item.iconSelected
+                                    else -> item.icon
+                                }, contentDescription = null
+                            )
+                        },
                         label = { Text(item.title) }
                     )
                 }
@@ -92,7 +106,7 @@ fun HomeScreen() {
         NavHost(
             modifier = Modifier.padding(it),
             navController = homeNavController,
-            startDestination = Routes.Home.Lateness
+            startDestination = Routes.Home.Setting
         ) {
             composable<Routes.Home.Attendance> { HomeAttendanceScreen() }
             composable<Routes.Home.Lateness> { HomeLatenessScreen() }
--- app/src/main/java/com/rizalanggoro/presensigo/presentation/home/setting/HomeSettingScreen.kt
+++ app/src/main/java/com/rizalanggoro/presensigo/presentation/home/setting/HomeSettingScreen.kt
@@ -10,12 +10,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.Logout
+import androidx.compose.material.icons.rounded.DeleteForever
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Tag
+import androidx.compose.material.icons.rounded.Upload
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -31,13 +39,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.composables.icons.lucide.LogOut
-import com.composables.icons.lucide.Lucide
-import com.composables.icons.lucide.RefreshCw
-import com.composables.icons.lucide.Upload
-import com.composables.icons.lucide.User
 import com.rizalanggoro.presensigo.BuildConfig
 import com.rizalanggoro.presensigo.core.Routes
 import com.rizalanggoro.presensigo.core.compositional.LocalNavController
@@ -88,7 +92,10 @@ fun HomeSettingScreen() {
             TopAppBar(title = { Text("Pengaturan") })
         }
     ) {
-        Column(modifier = Modifier.padding(it)) {
+        Column(
+            modifier = Modifier.padding(it),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
             // payload
             Row(
                 horizontalArrangement = Arrangement.spacedBy(16.dp),
@@ -102,9 +109,9 @@ fun HomeSettingScreen() {
                         .size(48.dp)
                 ) {
                     Icon(
-                        Lucide.User,
+                        Icons.Rounded.Person,
                         contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                        tint = MaterialTheme.colorScheme.primary,
                         modifier = Modifier.align(Alignment.Center)
                     )
                 }
@@ -119,61 +126,94 @@ fun HomeSettingScreen() {
                     }
                 }
             }
-            HorizontalDivider(modifier = Modifier.padding(top = 16.dp, bottom = 8.dp))
 
-            ListItem(
-                leadingContent = {
-                    Icon(
-                        Lucide.Upload,
-                        contentDescription = null
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Card(
+                    shape = RoundedCornerShape(
+                        topStart = MaterialTheme.shapes.large.topStart,
+                        topEnd = MaterialTheme.shapes.large.topEnd,
+                        bottomStart = MaterialTheme.shapes.small.bottomStart,
+                        bottomEnd = MaterialTheme.shapes.extraSmall.bottomEnd
                     )
-                },
-                headlineContent = { Text("Import data") },
-                supportingContent = { Text("Unggah data excel angkatan, kelas, dan siswa") },
-                modifier = Modifier.clickable {
-                }
-            )
-
-            ListItem(
-                leadingContent = {
-                    Icon(
-                        Lucide.RefreshCw,
-                        contentDescription = null,
+                ) {
+                    ListItem(
+                        colors = ListItemDefaults.colors(containerColor = Color.Transparent),
+                        leadingContent = {
+                            Icon(
+                                Icons.Rounded.Upload,
+                                contentDescription = null
+                            )
+                        },
+                        headlineContent = { Text("Unggah Data") },
+                        supportingContent = {
+                            Text(
+                                "Unggah data angkatan, jurusan, kelas, dan " +
+                                        "siswa dalam bentuk excel file"
+                            )
+                        },
+                        modifier = Modifier.clickable {
+                        }
                     )
-                },
-                headlineContent = { Text("Reset") },
-                supportingContent = { Text("Hapus semua data sekolah saat ini") },
-                modifier = Modifier.clickable {
-                    isResetDialogOpen = true
                 }
-            )
 
-            ListItem(
-                leadingContent = {
-                    Icon(
-                        Lucide.LogOut,
-                        contentDescription = null,
+                Card(
+                    shape = RoundedCornerShape(
+                        bottomStart = MaterialTheme.shapes.large.bottomStart,
+                        bottomEnd = MaterialTheme.shapes.large.bottomEnd,
+                        topStart = MaterialTheme.shapes.small.topStart,
+                        topEnd = MaterialTheme.shapes.extraSmall.topEnd
                     )
-                },
-                headlineContent = { Text("Keluar") },
-                modifier = Modifier.clickable {
-                    isLogoutDialogOpen = true
+                ) {
+                    ListItem(
+                        colors = ListItemDefaults.colors(containerColor = Color.Transparent),
+                        leadingContent = {
+                            Icon(
+                                Icons.Rounded.DeleteForever,
+                                contentDescription = null,
+                            )
+                        },
+                        headlineContent = { Text("Reset") },
+                        supportingContent = { Text("Hapus semua data sekolah saat ini") },
+                        modifier = Modifier.clickable {
+                            isResetDialogOpen = true
+                        }
+                    )
                 }
-            )
+            }
 
-            HorizontalDivider(modifier = Modifier.padding(top = 8.dp))
+            Card(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                shape = MaterialTheme.shapes.large
+            ) {
+                ListItem(
+                    colors = ListItemDefaults.colors(containerColor = Color.Transparent),
+                    leadingContent = {
+                        Icon(
+                            Icons.AutoMirrored.Rounded.Logout,
+                            contentDescription = null,
+                        )
+                    },
+                    headlineContent = { Text("Keluar") },
+                    modifier = Modifier.clickable {
+                        isLogoutDialogOpen = true
+                    }
+                )
+            }
 
-            Text(
-                "Lainnya",
-                style = MaterialTheme.typography.labelLarge.copy(
-                    color = MaterialTheme.colorScheme.primary
-                ),
-                modifier = Modifier.padding(start = 16.dp, top = 16.dp)
-            )
-            ListItem(
-                headlineContent = { Text("Release tag") },
-                supportingContent = { Text(BuildConfig.RELEASE_TAG) }
-            )
+            Card(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                shape = MaterialTheme.shapes.large
+            ) {
+                ListItem(
+                    colors = ListItemDefaults.colors(containerColor = Color.Transparent),
+                    leadingContent = { Icon(Icons.Rounded.Tag, contentDescription = null) },
+                    headlineContent = { Text("Release tag") },
+                    supportingContent = { Text(BuildConfig.RELEASE_TAG) }
+                )
+            }
         }
     }
 
