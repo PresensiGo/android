8a7980a9c0387fccf1c4ab36f11564d43d20db84
Rizal Anggoro <gnoogler4@gmail.com>
2025-07-29 20:51:02 +0700

    feat: detail lateness screen

--- app/src/main/java/com/rizalanggoro/presensigo/presentation/lateness/detail/DetailLatenessScreen.kt
+++ app/src/main/java/com/rizalanggoro/presensigo/presentation/lateness/detail/DetailLatenessScreen.kt
@@ -1,23 +1,38 @@
 package com.rizalanggoro.presensigo.presentation.lateness.detail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material.icons.rounded.PersonAdd
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import com.rizalanggoro.presensigo.core.Routes
 import com.rizalanggoro.presensigo.core.compositional.LocalNavController
+import com.rizalanggoro.presensigo.ui.theme.CardCornerShape
 import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -39,21 +54,65 @@ fun DetailLatenessScreen() {
             )
         },
         floatingActionButton = {
-            ExtendedFloatingActionButton(
+            FloatingActionButton(
                 onClick = {
                     navController.navigate(
                         Routes.Lateness.Detail.Create(
                             latenessId = viewModel.params.latenessId
                         )
                     )
-                },
-                shape = CircleShape,
-                icon = { Icon(Icons.Rounded.PersonAdd, contentDescription = null) },
-                text = { Text("Tambah siswa") },
-            )
-        },
-        floatingActionButtonPosition = FabPosition.Center
+                }
+            ) {
+                Icon(Icons.Rounded.PersonAdd, contentDescription = null)
+            }
+        }
     ) {
-        LazyColumn(modifier = Modifier.padding(it)) { }
+        LazyColumn(
+            modifier = Modifier.padding(it),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            items(10) {
+                StudentItem(index = it, totalCount = 10)
+            }
+            item {
+                Spacer(modifier = Modifier.height((56 + 32).dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun StudentItem(index: Int, totalCount: Int) {
+    val shape = CardCornerShape.getShape(CardCornerShape.getPosition(index, totalCount))
+
+    ElevatedCard(
+        shape = shape,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(shape)
+            .padding(horizontal = 16.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.secondaryContainer)
+            ) {
+                Icon(
+                    Icons.Rounded.Person,
+                    contentDescription = null,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+            Column {
+                Text("Rizal Dwi Anggoro", style = MaterialTheme.typography.titleMedium)
+                Text("130603 - MIPA 1", style = MaterialTheme.typography.bodyMedium)
+            }
+        }
     }
 }
