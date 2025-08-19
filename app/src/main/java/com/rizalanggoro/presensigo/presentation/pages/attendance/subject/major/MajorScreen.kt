a1a0e8554086a5d31a8c32189c1b0f28905c5e9f
Rizal Anggoro <gnoogler4@gmail.com>
2025-08-19 11:40:09 +0700

    chore: redesign major screen

--- app/src/main/java/com/rizalanggoro/presensigo/presentation/pages/attendance/subject/major/MajorScreen.kt
+++ app/src/main/java/com/rizalanggoro/presensigo/presentation/pages/attendance/subject/major/MajorScreen.kt
@@ -1,14 +1,26 @@
 package com.rizalanggoro.presensigo.presentation.pages.attendance.subject.major
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.automirrored.rounded.ArrowRightAlt
+import androidx.compose.material.icons.rounded.School
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,15 +28,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.rizalanggoro.presensigo.core.Routes
 import com.rizalanggoro.presensigo.core.compositional.LocalNavController
+import com.rizalanggoro.presensigo.core.constants.isLoading
+import com.rizalanggoro.presensigo.core.constants.isSuccess
 import com.rizalanggoro.presensigo.openapi.models.Major
+import com.valentinilk.shimmer.shimmer
 import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -36,32 +54,85 @@ fun MajorScreen() {
     val navController = LocalNavController.current
 
     Scaffold(
-        topBar = {
-            TopAppBar(title = { Text("Daftar Jurusan") }, navigationIcon = {
-                IconButton(
-                    onClick = {
-                        navController.popBackStack()
-                    }
-                ) {
-                    Icon(
-                        Icons.AutoMirrored.Rounded.ArrowBack, contentDescription = null
-                    )
-                }
-            })
-        }
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
     ) {
-        LazyColumn(
+        Column(
             modifier = Modifier.padding(it),
-            verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
-            items(state.majors) {
-                MajorItem(major = it) {
-                    navController.navigate(
-                        Routes.Attendance.Subject.Classroom(
-                            batchId = viewModel.params.batchId,
-                            majorId = it.id,
-                        )
-                    )
+            // top bar
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 12.dp)
+            ) {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(Icons.AutoMirrored.Rounded.ArrowBack, contentDescription = null)
+                }
+                Text(
+                    "Pilih Jurusan",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+
+            // content
+            PullToRefreshBox(
+                isRefreshing = state.status.isLoading(),
+                onRefresh = { viewModel.getAllMajors() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .clip(RoundedCornerShape(topEnd = 24.dp, topStart = 24.dp))
+                    .background(MaterialTheme.colorScheme.background),
+            ) {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    item {
+                        Column(
+                            modifier = Modifier.padding(
+                                start = 24.dp,
+                                end = 24.dp,
+                                top = 24.dp,
+                                bottom = 8.dp
+                            )
+                        ) {
+                            Text(
+                                "Daftar Jurusan",
+                                style = MaterialTheme.typography.titleMedium,
+                                color = MaterialTheme.colorScheme.onBackground
+                            )
+                            Text(
+                                "Silahkan pilih jurusan yang akan dilakukan presensi mata pelajaran.",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = MaterialTheme.colorScheme.onBackground.copy(alpha = .8f)
+                            )
+                        }
+                    }
+
+                    // loading state
+                    if (state.status.isLoading())
+                        items(3) {
+                            MajorItem(isLoading = true)
+                        }
+
+                    // success state
+                    if (state.status.isSuccess())
+                        items(state.majors) {
+                            MajorItem(major = it) {
+                                navController.navigate(
+                                    Routes.Attendance.Subject.Classroom(
+                                        batchId = viewModel.params.batchId,
+                                        majorId = it.id,
+                                    )
+                                )
+                            }
+                        }
+
+                    item {
+                        Spacer(modifier = Modifier.height(24.dp))
+                    }
                 }
             }
         }
@@ -69,15 +140,120 @@ fun MajorScreen() {
 }
 
 @Composable
-private fun MajorItem(major: Major, onClick: () -> Unit) {
+private fun MajorItem(
+    isLoading: Boolean = false,
+    major: Major? = null,
+    onClick: () -> Unit = {}
+) {
     OutlinedCard(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp)
-            .clickable { onClick() }
+            .padding(horizontal = 24.dp)
+            .clip(CardDefaults.outlinedShape)
+            .clickable(enabled = isLoading == false) { onClick() }
     ) {
-        Column(modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp)) {
-            Text(major.name, style = MaterialTheme.typography.titleMedium)
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .then(
+                        when (isLoading) {
+                            true -> Modifier
+                                .shimmer()
+                                .background(MaterialTheme.colorScheme.outlineVariant)
+
+                            else -> Modifier.background(
+                                MaterialTheme.colorScheme.onPrimaryContainer.copy(
+                                    alpha = .48f
+                                )
+                            )
+                        }
+                    )
+            ) {
+                Icon(
+                    Icons.Rounded.School,
+                    contentDescription = null,
+                    modifier = Modifier.align(Alignment.Center),
+                    tint = when (isLoading) {
+                        true -> MaterialTheme.colorScheme.outlineVariant
+                        else -> MaterialTheme.colorScheme.primaryContainer
+                    }
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f), verticalArrangement = Arrangement.spacedBy(
+                        when (isLoading) {
+                            true -> 4.dp
+                            else -> 0.dp
+                        }
+                    )
+                ) {
+                    Text(
+                        major?.name ?: "loading nama jurusan",
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = Modifier.then(
+                            when (isLoading) {
+                                true -> Modifier
+                                    .shimmer()
+                                    .clip(CircleShape)
+                                    .background(MaterialTheme.colorScheme.outlineVariant)
+
+                                else -> Modifier
+                            }
+                        ),
+                        color = when (isLoading) {
+                            true -> MaterialTheme.colorScheme.outlineVariant
+                            else -> Color.Unspecified
+                        }
+                    )
+                    Text(
+                        "5 Kelas",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.then(
+                            when (isLoading) {
+                                true -> Modifier
+                                    .shimmer()
+                                    .clip(CircleShape)
+                                    .background(MaterialTheme.colorScheme.outlineVariant)
+
+                                else -> Modifier
+                            }
+                        ),
+                        color = when (isLoading) {
+                            true -> MaterialTheme.colorScheme.outlineVariant
+                            else -> MaterialTheme.colorScheme.onBackground.copy(alpha = .8f)
+                        }
+                    )
+                }
+
+                Icon(
+                    Icons.AutoMirrored.Rounded.ArrowRightAlt,
+                    contentDescription = null,
+                    modifier = Modifier.then(
+                        when (isLoading) {
+                            true -> Modifier
+                                .shimmer()
+                                .clip(CircleShape)
+                                .background(MaterialTheme.colorScheme.outlineVariant)
+
+                            else -> Modifier
+                        }
+                    ),
+                    tint = when (isLoading) {
+                        true -> MaterialTheme.colorScheme.outlineVariant
+                        else -> MaterialTheme.colorScheme.outline
+                    }
+                )
+            }
         }
     }
 }
